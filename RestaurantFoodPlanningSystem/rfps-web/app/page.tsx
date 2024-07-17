78cd6926bb8340e500a1bb1d27476b5f41701009
Chi Ho Ho <cho127@my.centennialcollege.ca>
2024-07-16 20:42:55 -0400

    Update landing page

--- RestaurantFoodPlanningSystem/rfps-web/app/page.tsx
+++ RestaurantFoodPlanningSystem/rfps-web/app/page.tsx
@@ -1,14 +1,22 @@
-import {Link} from "@nextui-org/react";
+import {Button, Link} from "@nextui-org/react";
 
 
 export default function Home() {
     return (
         <>
-            <div className={"flex flex-col items-center justify-center h-screen w-screen"}>
-                <Link href={"/login"} 
-                      isBlock={true} 
-                      underline={"hover"}
-                >Login</Link>
+            <div className={"flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-r from-sky-500 to-indigo-500"}>
+                <h1 className={"font-serif text-white text-6xl m-4"}>Welcome to Restaurant Food Planning System</h1>
+                <Button href={"/login"}
+                        as={Link}
+                        color={"warning"}
+                        showAnchorIcon={true}
+                        variant={"ghost"}
+                        className={"font-serif h-fit p-4 text-4xl"}
+                        size={"lg"}
+                        
+                >
+                    Login
+                </Button>
             </div>
         </>
     );
